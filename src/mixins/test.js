d5d3b75390306f3617751aef5d66c144e48833ea
clannad <719462227@qq.com>
2018-02-13 13:41:34 +0800

    完成自定义统计

--- src/mixins/test.js
+++ src/mixins/test.js
@@ -2,6 +2,7 @@ import wepy from 'wepy'
 
 export default class testMixin extends wepy.mixin {
   data = {
+    height: '',
     mixin: 'This is mixin data.'
   }
   methods = {
@@ -10,12 +11,26 @@ export default class testMixin extends wepy.mixin {
       console.log('mixin method tap')
     }
   }
-
-  onShow() {
-    console.log('mixin onShow')
-  }
+  // onShow() {
+  //   console.log('mixin onShow')
+  // }
 
   onLoad() {
-    console.log('mixin onLoad')
+    wepy.getSystemInfo({
+      success: (res) => {
+        this.height = res.windowHeight
+      }
+    })
+  }
+  postMethod(url, data, call) {
+    wepy.request({
+      url: this.$parent.globalData.apiUrl + url,
+      method: 'POST',
+      success(res) {
+        if (res.data.isSuccess) {
+          call(res.data.retEntity)
+        }
+      }
+    })
   }
 }
